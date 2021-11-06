799e29c43dea23803df3cfe80faef4a2941f8c48
Robert Hardy <helloarchy@gmail.com>
2021-11-06 10:57:23 +0000

    Initial commit * Generate main and test projects * Add Product and Special Offer classes

---
+++ GoodGroceries/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GoodGroceries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Initialise products */
+            var BREAD = new Product()
+            {
+                Name = "Bread",
+                Price = 1.10M
+            };
+
+            var MILK = new Product()
+            {
+                Name = "Milk",
+                Price = 0.50M
+            };
+
+            var CHEESE = new Product()
+            {
+                Name = "Cheese",
+                Price = 0.90M
+            };
+
+            var SOUP = new Product()
+            {
+                Name = "Soup",
+                Price = 0.60M
+            };
+            
+            
+
+        }
+    }
+}
---
+++ GoodGroceries/SpecialOffer.cs
@@ -0,0 +1,16 @@
+﻿namespace GoodGroceries
+{
+    public class SpecialOffer
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal DiscountPercentage { get; set; }
+
+        public SpecialOffer(Product product, int quantity, decimal discountPercentage)
+        {
+            Product = product;
+            Quantity = quantity;
+            DiscountPercentage = discountPercentage;
+        }
+    }
+}
